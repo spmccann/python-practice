b1b6c0bcae2b93b604e8aa7140daaca5be0a8e16
Scott <scottpmccann@gmail.com>
2022-04-03 15:03:23 -0700

    collisions

--- breakout/breakout.py
+++ breakout/breakout.py
@@ -1,9 +1,6 @@
 from turtle import Turtle, Screen
 import random
-
-screen = Screen()
-screen.setup(width=800, height=600)
-brick_colors = "red", "blue", "yellow", "green", "purple", "orange"
+import time
 
 
 class Paddle(Turtle):
@@ -13,16 +10,18 @@ class Paddle(Turtle):
         self.paddle.penup()
         self.paddle.hideturtle()
         self.paddle.shape("square")
-        self.paddle.shapesize(1, 6)
+        self.paddle.shapesize(1, 5)
         self.paddle.color("blue")
         self.paddle.setposition(0, -250)
         self.paddle.showturtle()
 
     def pad_left(self):
-        self.paddle.fd(-100)
+        self.paddle.fd(-50)
+        print(self.paddle.pos())
 
     def pad_right(self):
-        self.paddle.fd(100)
+        self.paddle.fd(50)
+        print(self.paddle.pos())
 
 
 class Ball(Turtle):
@@ -32,7 +31,6 @@ class Ball(Turtle):
         self.ball.penup()
         self.ball.shape("circle")
         self.ball.color("black")
-        self.ball.right(90)
         self.x = 0
         self.y = 0
         self.ball.setposition(self.x, self.y)
@@ -40,14 +38,23 @@ class Ball(Turtle):
     def restart(self):
         self.ball.hideturtle()
         self.ball.setposition(0, 0)
+        self.ball.setheading(270)
         self.ball.showturtle()
 
     def move(self):
-        self.ball.fd(50)
+        self.ball.fd(40)
 
-    def bounds(self):
-        if -375 < self.ball.xcor() < 375 and -275 < self.ball.ycor() < 275:
-            return True
+    def bounce(self):
+        if self.ball.xcor() > 365:
+            self.ball.right(self.ball.xcor() * -1)
+        elif self.ball.xcor() < -375:
+            self.ball.right(self.ball.xcor() * 1)
+        elif self.ball.distance(paddle.paddle.pos()) < 50:
+            self.ball.right((self.ball.ycor() * -1))
+        elif self.ball.ycor() < -265:
+            Ball.restart(self)
+        elif self.ball.ycor() > 250:
+            self.ball.right(self.ball.ycor() * -1)
 
 
 class Brick:
@@ -59,26 +66,28 @@ class Brick:
         self.brick.speed(0)
         self.brick.penup()
         self.brick.shape("square")
-        self.brick.color("blue")
+        self.brick_colors = "red", "blue", "yellow", "green", "purple", "orange"
         self.brick.shapesize(1, 3)
         self.brick.setposition(self.x, self.y)
         self.brick.showturtle()
 
     def wall(self):
-        for b in range(0, 5):
+        for b in range(0, 6):
             for br in range(0, 9):
-                if br == 1:
-                    rnd_color = random.choice(brick_colors)
-                    self.brick.color(rnd_color)
-                self.brick.stamp()
                 self.brick.penup()
                 self.brick.setposition(self.x, self.y)
                 self.brick.pendown()
+                rnd_color = random.choice(self.brick_colors)
+                self.brick.color(rnd_color)
                 self.brick.stamp()
                 self.x -= 75
             self.x = 300
             self.y -= 25
 
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.tracer(0)
 
 paddle = Paddle()
 ball = Ball()
@@ -90,11 +99,16 @@ screen.onkey(paddle.pad_right, "d")
 screen.listen()
 
 game = True
+new_game = True
 while game:
-    if ball.bounds():
-        ball.move()
-    else:
+    screen.update()
+    time.sleep(0.1)
+    if new_game:
         ball.restart()
+        new_game = False
+    else:
+        ball.move()
+        ball.bounce()
 
 
 screen.mainloop()
